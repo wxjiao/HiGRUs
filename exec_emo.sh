ef90d1befe9b0b3b0db6bca6cd1b57bf5d776219
Jiao, Wenxiang <31032829+wxjiao@users.noreply.github.com>
2019-03-17 10:58:44 +0800

    Update exec_emo.sh

--- exec_emo.sh
+++ exec_emo.sh
@@ -8,5 +8,17 @@ echo ========= lr=$LR ==============
 for iter in 1 2 3 4 5
 do
 echo --- $Enc - $Dec $iter ---
-python EmoMain.py -lr $LR -gpu $GPU -type higru-sf -d_h1 $du -d_h2 $dc -report_loss 720 -data_path Friends_data.pt -vocab_path Friends_vocab.pt -emodict_path Friends_emodict.pt -tr_emodict_path Friends_tr_emodict.pt -dataset Friends -embedding Friends_embedding.pt
+python EmoMain.py \
+-lr $LR \
+-gpu $GPU \
+-type higru-sf \
+-d_h1 $du \
+-d_h2 $dc \
+-report_loss 720 \
+-data_path Friends_data.pt \
+-vocab_path Friends_vocab.pt \
+-emodict_path Friends_emodict.pt \
+-tr_emodict_path Friends_tr_emodict.pt \
+-dataset Friends \
+-embedding Friends_embedding.pt
 done
