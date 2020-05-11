c46a9b9933ff93ddbda20b9660e0c50ae6ca97f1
Jiao, Wenxiang <31032829+wxjiao@users.noreply.github.com>
2020-05-11 21:35:07 +0800

    Update Visualize.py

--- Visualize.py
+++ Visualize.py
@@ -21,39 +21,39 @@ def main():
 	parser.add_argument('-decay', type=float, default=math.pow(0.5, 1/20))	# half lr every 20 epochs
 	parser.add_argument('-epochs', type=int, default=200)		# Defualt epochs 200
 	parser.add_argument('-patience', type=int, default=10,		# Patience of early stopping 10 epochs
-	                    help='patience for early stopping')
+			    help='patience for early stopping')
 	parser.add_argument('-save_dir', type=str, default="snapshot",	# Save the model and results in snapshot/
-	                    help='where to save the models')
+			    help='where to save the models')
 	# Data
 	parser.add_argument('-dataset', type=str, default='Friends',	# Default dataset Friends
-	                    help='dataset')
+			    help='dataset')
 	parser.add_argument('-data_path', type=str, required = True,
-	                    help='data path')
+			    help='data path')
 	parser.add_argument('-vocab_path', type=str, required=True,
-	                    help='vocabulary path')
+			    help='vocabulary path')
 	parser.add_argument('-emodict_path', type=str, required=True,
-	                    help='emotion label dict path')
+			    help='emotion label dict path')
 	parser.add_argument('-tr_emodict_path', type=str, default=None,
-	                    help='training set emodict path')
+			    help='training set emodict path')
 	parser.add_argument('-max_seq_len', type=int, default=80,	# Pad each utterance to 80 tokens
-	                    help='the sequence length')
+			    help='the sequence length')
 	# model
 	parser.add_argument('-type', type=str, default='higru', 	# Model type: default HiGRU
-	                    help='choose the low encoder')
+			    help='choose the low encoder')
 	parser.add_argument('-d_word_vec', type=int, default=300,	# Embeddings size 300
-	                    help='the word embeddings size')
+			    help='the word embeddings size')
 	parser.add_argument('-d_h1', type=int, default=300,		# Lower-level RNN hidden state size 300
-	                    help='the hidden size of rnn1')
+			    help='the hidden size of rnn1')
 	parser.add_argument('-d_h2', type=int, default=300,		# Upper-level RNN hidden state size 300
-	                    help='the hidden size of rnn1')
+			    help='the hidden size of rnn1')
 	parser.add_argument('-d_fc', type=int, default=100,		# FC size 100
-	                    help='the size of fc')
+			    help='the size of fc')
 	parser.add_argument('-gpu', type=str, default=None,		# Spcify the GPU for training
-	                    help='gpu: default 0')
+			    help='gpu: default 0')
 	parser.add_argument('-embedding', type=str, default=None,	# Stored embedding path
-	                    help='filename of embedding pickle')
+			    help='filename of embedding pickle')
 	parser.add_argument('-report_loss', type=int, default=720,	# Report loss interval, default the number of dialogues
-	                    help='how many steps to report loss')
+			    help='how many steps to report loss')
 
 	args = parser.parse_args()
 	print(args, '\n')
@@ -85,13 +85,13 @@ def main():
 
 	# Choose the model
 	model = HiGRU(d_word_vec=args.d_word_vec,
-	              d_h1=args.d_h1,
-	              d_h2=args.d_h2,
-	              d_fc=args.d_fc,
-	              emodict=emodict,
-	              worddict=worddict,
-	              embedding=embedding,
-	              type=args.type)
+		      d_h1=args.d_h1,
+		      d_h2=args.d_h2,
+		      d_fc=args.d_fc,
+		      emodict=emodict,
+		      worddict=worddict,
+		      embedding=embedding,
+		      type=args.type)
 
 	# Choose focused emotions
 	focus_emo = Const.four_emo
@@ -111,15 +111,15 @@ def main():
 	print("Load best models for testing!")
 	model = Utils.model_loader(args.save_dir, args.type, args.dataset)
 	pAccs = emoeval(model=model,
-	                data_loader=test_loader,
-	                tr_emodict=tr_emodict,
-	                emodict=emodict,
-	                args=args,
-	                focus_emo=focus_emo)
+			data_loader=test_loader,
+			tr_emodict=tr_emodict,
+			emodict=emodict,
+			args=args,
+			focus_emo=focus_emo)
 	print("Test: ACCs-WA-UWA {}".format(pAccs))
 
 	# Take the pre-softmax weights
- 	presoft = model.classifier[-1].weight
+	presoft = model.classifier[-1].weight
 	print(presoft.size())
 
 
